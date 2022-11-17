8d970906a1f00ead95955a1a224e4f8a499251b7
angelo <a.lazzaretti@greenaus.it>
2022-11-17 14:54:37 +0100

    aggiunta di esempio settaggio parametro in xhtml

--- src/main/java/it/trionica/web/controller/UserController.java
+++ src/main/java/it/trionica/web/controller/UserController.java
@@ -6,23 +6,50 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ComponentSystemEvent;
 
 import it.trionica.web.util.Util;
 import lombok.extern.log4j.Log4j2;
 
-@ManagedBean(name="personaInsertController")
+@ManagedBean(name="userController")
 @ViewScoped
 @Log4j2
 public class UserController implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	private String nomeUtente;
+	
+	
 	@ManagedProperty(value="#{util}")
 	private Util util;
 	
 	@PostConstruct
 	public void init() throws Exception{
+		
 		log.debug("UserController init");	
+	}
+	
+	public void onLoadView(ComponentSystemEvent event) {
+	
+		log.debug("sono in onloadView");
+		setNomeUtente("Angelo");
+	}
+
+	public String getNomeUtente() {
+		return nomeUtente;
+	}
+
+	public void setNomeUtente(String nomeUtente) {
+		this.nomeUtente = nomeUtente;
+	}
+
+	public Util getUtil() {
+		return util;
+	}
+
+	public void setUtil(Util util) {
+		this.util = util;
 	}
 	
 
