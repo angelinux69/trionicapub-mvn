df5b04182ebb0036998d90e56150e3437ec309d6
Desmocris <cristian.cirulli.93@gmail.com>
2022-12-13 15:27:57 +0100

    userLog e token

--- src/main/java/it/trionica/web/model/dto/user/UserDTO.java
+++ src/main/java/it/trionica/web/model/dto/user/UserDTO.java
@@ -3,7 +3,6 @@ package it.trionica.web.model.dto.user;
 import lombok.Data;
 import lombok.ToString;
 
-@Data
 @ToString(includeFieldNames=true)
 public class UserDTO {
 	
@@ -13,5 +12,42 @@ public class UserDTO {
 	protected String nome; 
 	protected String cognome;
 	protected String ruolo;
-
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getRuolo() {
+		return ruolo;
+	}
+	public void setRuolo(String ruolo) {
+		this.ruolo = ruolo;
+	}
+	
 }
